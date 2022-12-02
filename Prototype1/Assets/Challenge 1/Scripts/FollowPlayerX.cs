e370984a93730586b661027f57063b769f3b96b7
Doufa <douglas.neves.5205@gmail.com>
2022-12-01 22:32:55 -0300

    Challenge 1

---
+++ Prototype1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(15f,2f,0f);
+
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
